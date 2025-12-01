f066f65bfbaa6c195fa135c7a14db62807d761ba
Taran234 <ts406093@gmail.com>
2025-12-01 09:07:18 +0530

    sticky size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" />
 
       </head>
