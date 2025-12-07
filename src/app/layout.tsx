2bd21184d80a4aab7715f6078c079ed127c1fa46
Kahanhaikaran <opkabal415@gmail.com>
2025-12-07 12:04:36 +0530

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
-
 import "../styles/index.scss";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Shiksha Sagar - Academic Support Platform",
+	description: "Personalized academic support for assignments, projects, and exams.",
+};
 
 export default function RootLayout({
 	children,
@@ -9,9 +14,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" />
-
       </head>
 			<body>{children}</body>
 		</html>
